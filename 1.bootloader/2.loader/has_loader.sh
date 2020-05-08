370c48c8a74b801f66f148cebaba95b4eb8baec3
meilihao <563278383@qq.com>
2020-05-08 14:06:03 +0800

    . clean 2.loader

--- 1.bootloader/2.loader/has_loader.sh
+++ 1.bootloader/2.loader/has_loader.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+# floppy_loader.img : copy from 1.boot's floppy.img
 as  -o boot.o boot.s
 ld -Ttext 0x7c00 --oformat=binary boot.o -o boot.bin
 dd if=boot.bin of=floppy_loader.img  bs=512 count=1 conv=notrunc
-# bochs -f bochsrc_loader.txt
+
+as  -o loader.o loader.s
+ld -Ttext 0x0 --oformat=binary loader.o -o loader.bin
+sudo mount floppy_loader.img ./fat
+sudo cp loader.bin fat
+sudo umount fat
+bochs -f bochsrc_loader.txt
