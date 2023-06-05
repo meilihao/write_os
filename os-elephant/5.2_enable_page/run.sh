0003ed48d9013db43d9d558a58fb335d2e7bf8c1
meilihao <563278383@qq.com>
2023-06-05 17:59:28 +0800

    chore(kernel): add kernel

--- os-elephant/5.2_enable_page/run.sh
+++ os-elephant/5.2_enable_page/run.sh
@@ -2,4 +2,6 @@
 #bximage -func=create -hd=16M -imgmode="flat" -sectsize=512 -q hd.img # size<10会报`Hard disk image size out of range`
 #nasm -I include/ -o mbr.bin mbr.s && dd if=mbr.bin of=./hd.img bs=512 count=1  conv=notrunc
 nasm -I include/ -o loader.bin loader.s && dd if=loader.bin of=./hd.img bs=512 count=4 seek=2 conv=notrunc
+gcc -c -o kernel/main.o kernel/main.c
+ld kernel/main.o -Ttext 0xc0001500 -e main -o kernel/kernel.bin
 qemu-system-i386 -hda hd.img
